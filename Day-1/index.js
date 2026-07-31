8d36978eff4769326884fe93f77d774c07479d94
Anivaran Dubey <aniv8007@gmail.com>
2026-07-31 11:06:09 +0530

    initial commit

---
+++ Day-1/index.js
@@ -0,0 +1,19 @@
+const app= require('./app.js');
+// var a=20;
+// var b=30;
+// var c=50;
+// console.log(a+b+c);
+// 
+// var x=2;
+// if(x<=2){
+// console.log("X is 2")
+// }
+// for(var i=0;i<5;i++){
+//     console.log(i);
+// }
+const arr=[1,2,3,2,3];
+for(var i=1;i<arr.length;i++){
+    console.log(arr[i]);
+}
+console.log(app.xz);
+console.log(app.yy);
---
+++ Day-2/A.js
@@ -0,0 +1,6 @@
+//Variables
+console.log("===== Variables =====");
+var name="Anivaran";
+var surname="Dubey";
+console.log(name);
+console.log(surname)
